c312512949cef9a4a02ce0470d1f2f547d853b11
Garbulix <garbiec@bartek.me>
2020-11-13 23:16:06 +0100

    palindrome check optimizations

--- palindrome-check/palindrome_check.py
+++ palindrome-check/palindrome_check.py
@@ -1,37 +1,16 @@
-def is_even(number):
-    if (number % 2) == 0:
-        return True
-    else:
-        return False
+
 
 def reversed(some_string):
     reversed_string = ''
-    last_letter_index = len(some_string)-1
+    last_letter_index = len(some_string) - 1
 
-    for letter_index in range(last_letter_index, -1, -1):
-        reversed_string = reversed_string + some_string[letter_index]
+    for current_index in range(len(some_string)):
+        reversed_string = reversed_string + some_string[last_letter_index - current_index]
 
     return reversed_string
 
-def check_palindrome(given_string):
-    testing_string = given_string
-
-    str_length = len(testing_string)
-    halfed_string = None
-    middle_letter = None
-    middle_letter_index = None
-    made_up_palindrome = None
-
-    if (is_even(str_length)):
-        halfed_string = testing_string[:int(str_length / 2)]
-        made_up_palindrome = halfed_string + reversed(halfed_string)
+def check_palindrome(some_string):
+    if len(some_string) < 2:
+        return False
     else:
-        middle_letter_index = int((str_length - 1) / 2)
-        middle_letter = testing_string[middle_letter_index]
-        halfed_string = testing_string[:middle_letter_index] # letters before middle letter (except middle letter)
-        made_up_palindrome = halfed_string + middle_letter + reversed(halfed_string)
-
-    return made_up_palindrome.upper() == given_string.upper()   # should be case insensitive
-
-
-print(check_palindrome('Kajak'))
+        return some_string.upper() == reversed(some_string.upper())   # should be case insensitive
