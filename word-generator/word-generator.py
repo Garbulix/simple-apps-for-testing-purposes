cd5e28812d6568f1670d6baba37fbc564d776585
Garbulix <garbiec@bartek.me>
2020-12-17 23:38:06 +0100

    comment update

--- word-generator/word-generator.py
+++ word-generator/word-generator.py
@@ -5,17 +5,17 @@ consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's
 
 def generate_word(length):
     generated_word = ''
-    # define to start with vowel or consonant
+    # randomly choose to start with vowel or consonant
     random.seed()
     start_with = random.choice(['vowel', 'consonant'])
     generated_word += random.choice(vowel if (start_with == 'vowel') else consonant)
     previous_letter = start_with
     for current_letter in range(1, length):
-        # we already have first letter
         generated_word += random.choice(consonant if (previous_letter == 'vowel') else vowel)
         previous_letter = 'consonant' if (previous_letter == 'vowel') else 'vowel'
     return generated_word
 
+# examplary use 
 random.seed()
 for x in range(8):
     print(generate_word(random.randint(3, 15)))
