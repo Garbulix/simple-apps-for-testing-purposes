b27163acb0931f8ebba330fc53d4ced8893c37e7
Garbulix <garbiec@bartek.me>
2020-12-17 23:47:59 +0100

    added word list creating function

--- word-generator/word-generator.py
+++ word-generator/word-generator.py
@@ -4,6 +4,10 @@ vowel = ['a', 'e', 'i', 'o', 'u', 'y']
 consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'w', 'z']
 
 def generate_word(length):
+    # there is no word to create
+    if length == 0:
+        return ''
+    
     generated_word = ''
     # randomly choose to start with vowel or consonant
     random.seed()
@@ -15,7 +19,13 @@ def generate_word(length):
         previous_letter = 'consonant' if (previous_letter == 'vowel') else 'vowel'
     return generated_word
 
-# examplary use 
-random.seed()
-for x in range(8):
-    print(generate_word(random.randint(3, 15)))
+def generate_word_list(no_of_words, min_chars_in_word, max_chars_in_word):
+    random.seed()
+    words_list = []
+    for x in range(no_of_words):
+        words_list.append(generate_word(random.randint(min_chars_in_word, max_chars_in_word)))
+    return words_list
+
+# ===================================
+
+print(generate_word_list(5, 4, 8))
